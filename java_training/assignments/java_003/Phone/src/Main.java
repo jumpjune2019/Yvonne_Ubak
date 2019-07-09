8dc4455c0be7d32bf5a055b4e21662ffd030db1e
Idara Ubak <studentubak@gmail.com>
2019-07-09 10:12:23 -0500

    Java 003 Homework

--- java_training/assignments/java_003/Phone/src/Main.java
+++ java_training/assignments/java_003/Phone/src/Main.java
@@ -5,21 +5,26 @@ public class Main {
 
 	//phone records entry
 	static void phoneEntry() throws NullEntryException {
+		String [][]phoneArr = new String[5][3];
 		Scanner uIn = new Scanner(System.in);
 		Scanner aQ = new Scanner(System.in);
 		String menuSelect;
 		String uName;
 		String uPhone;
 		String uCity;
+		int x=0;
 		
 		for(int i = 0; i< 5; i++) {
 			try {
 				System.out.print("Name: ");
 				uName = uIn.nextLine();
+				verifyInput(uName);
 				System.out.print("\nPhone: ");
 				uPhone = uIn.nextLine();
+				verifyInput(uPhone);
 				System.out.print("\nCity: ");
 				uCity = uIn.nextLine();
+				verifyInput(uCity);
 				
 				//feedback
 				System.out.println("Name: " + uName + " Phone: "+ uPhone + " City: " + uCity);
@@ -28,54 +33,93 @@ public class Main {
 				menuSelect = aQ.nextLine();
 				
 				//accept
-				if(menuSelect == "1") {
-					printConsole();
-					printFile();
+				if(menuSelect.equals("1")) {
+					phoneArr[i][x] = uName;
+					phoneArr[i][x+1] = uPhone;
+					phoneArr[i][x+2] = uCity;
 				}
-				else if(menuSelect == "2") {
+				else if(menuSelect.equals("2")) {
 					uName = null;
 					uPhone = null;
 					uCity = null;
+					i--;
 					continue;
 				}
 				
 			}catch(Exception ne) {
-				throw new NullEntryException();
-				//continue;
+				i--;
+				continue;
+				
 			}
 		}
+		printConsole(phoneArr);
+		printFile(phoneArr);
 	}
 	
 	//display on console
-	static void printConsole() {
-		PhoneData pd = new PhoneData();
-		System.out.println(pd.tableData());
+	static void printConsole(String [][] phoneRecords) {
+		//2D array
+		//phoneRecords = new String[5][3];		//[row][column]
+				
+		System.out.println("Phonebook Data");
+		String format ="%-30s";
+		System.out.printf(format, "Name");
+		System.out.printf(format, "Phone");
+		System.out.printf(format, "City");
+		System.out.println();
+				
+		//add each row
+		for(int i=0; i<5; i++) {
+			for(int x=0; x<3;x++) {
+				System.out.printf(format, phoneRecords[i][x]);
+			}
+			System.out.println();
+		}
 	}
 		
 	//write to file
-	static void printFile() {
-		PhoneData pd = new PhoneData();
-		String data = pd.tableData();
+	static void printFile(String [][] phoneRecords) {
+		//2D array
+		//phoneRecords = new String[5][3];		//[row][column]
 		File dFile = new File("phone.txt");
-		FileWriter printTo = null;
+		PrintWriter printTo = null;
 		
 		try {
-			printTo = new FileWriter(dFile);
-			printTo.write(data);
+			printTo = new PrintWriter(dFile);
+			printTo.write("Phonebook Data");
+			printTo.println();
+			printTo.append(String.format("%-30s", "Name"));
+			printTo.append(String.format("%-30s", "Phone"));
+			printTo.append(String.format("%-30s", "City"));
+			printTo.println();
+			
+			//add each row
+			for(int i=0; i<5; i++) {
+				//columns
+				for(int x=0; x<3;x++) {
+					printTo.append(String.format("%-30s",phoneRecords[i][x]));
+				}
+				printTo.println();
+			}
 			
 		}catch(IOException e) {
-			e.printStackTrace();
+			System.out.println("Could not create file");
+			//e.printStackTrace();
 		}finally {
-			try {
 				printTo.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		
 			
 	}
 	
+	//ensure that entry ins't null
+	static void verifyInput(String s) throws NullEntryException{
+		if(s.equals("")) {
+			throw new NullEntryException("You did not enter a value. Please try again.");
+		}
+	}
+	
+	//application
 	public static void main(String[] args) throws NullEntryException {
 		// TODO Auto-generated method stub
 		
